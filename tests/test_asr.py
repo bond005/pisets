dab925197897313adefe008909bab63362442f70
Ivan Bondarenko <bond005@yandex.ru>
2024-05-01 16:52:58 +0700

    Oscillatory gallucinations removing for Whisper is implemented.

--- tests/test_asr.py
+++ tests/test_asr.py
@@ -7,11 +7,13 @@ try:
     from asr.asr import select_word_groups
     from asr.asr import split_long_segments
     from asr.asr import strip_segments
+    from asr.asr import find_repeated_tokens, find_tokens_in_text, remove_oscillatory_hallucinations
 except:
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
     from asr.asr import select_word_groups
     from asr.asr import split_long_segments
     from asr.asr import strip_segments
+    from asr.asr import find_repeated_tokens, find_tokens_in_text, remove_oscillatory_hallucinations
 
 
 class TestASR(unittest.TestCase):
@@ -164,6 +166,150 @@ class TestASR(unittest.TestCase):
         predicted_groups = select_word_groups(words, segment_size)
         self.assertIsInstance(predicted_groups, list)
         self.assertEqual(len(predicted_groups), len(target_groups))
+
+    def test_find_repeated_tokens_pos01(self):
+        tokens = ['мама', 'мыла', 'раму']
+        self.assertIsNone(find_repeated_tokens(tokens, 0))
+
+    def test_find_repeated_tokens_pos02(self):
+        tokens = ['мама', 'мыла', 'раму']
+        self.assertIsNone(find_repeated_tokens(tokens, 2))
+
+    def test_find_repeated_tokens_pos03(self):
+        tokens = ['мама', 'мыла', 'раму', 'раму', 'раму', 'раму', 'раму']
+        true_bounds = (2, 7)
+        res = find_repeated_tokens(tokens, 0)
+        self.assertIsInstance(res, tuple)
+        self.assertEqual(len(res), 2)
+        self.assertIsInstance(res[0], int)
+        self.assertIsInstance(res[1], int)
+        self.assertEqual(res, true_bounds)
+
+    def test_find_repeated_tokens_pos04(self):
+        tokens = ['мама', 'мама', 'мама', 'мама', 'мыла', 'раму', 'раму', 'раму', 'раму', 'раму']
+        true_bounds = (0, 4)
+        res = find_repeated_tokens(tokens, 0)
+        self.assertIsInstance(res, tuple)
+        self.assertEqual(len(res), 2)
+        self.assertIsInstance(res[0], int)
+        self.assertIsInstance(res[1], int)
+        self.assertEqual(res, true_bounds)
+
+    def test_find_repeated_tokens_pos05(self):
+        tokens = ['мама', 'мама', 'мама', 'мама', 'мыла', 'раму', 'раму', 'раму', 'раму', 'раму']
+        true_bounds = (5, 10)
+        res = find_repeated_tokens(tokens, 2)
+        self.assertIsInstance(res, tuple)
+        self.assertEqual(len(res), 2)
+        self.assertIsInstance(res[0], int)
+        self.assertIsInstance(res[1], int)
+        self.assertEqual(res, true_bounds)
+
+    def test_find_repeated_tokens_pos06(self):
+        tokens = ['мама', 'мыла', 'раму', 'раму', 'раму', 'раму', 'раму']
+        self.assertIsNone(find_repeated_tokens(tokens, 4))
+
+    def test_find_tokens_in_text_pos01(self):
+        s = 'Мама мыла раму.'
+        tokens = ['мама', 'мыла']
+        true_bounds = (0, 9)
+        res = find_tokens_in_text(s, tokens)
+        self.assertIsInstance(res, tuple)
+        self.assertEqual(len(res), 2)
+        self.assertIsInstance(res[0], int)
+        self.assertIsInstance(res[1], int)
+        self.assertEqual(res, true_bounds)
+
+    def test_find_tokens_in_text_pos02(self):
+        s = 'Мама, мы все тяжело больны. Мама, я знаю, мы все сошли с ума.'
+        tokens = ['мама', 'мы']
+        true_bounds = (0, 8)
+        res = find_tokens_in_text(s, tokens)
+        self.assertIsInstance(res, tuple)
+        self.assertEqual(len(res), 2)
+        self.assertIsInstance(res[0], int)
+        self.assertIsInstance(res[1], int)
+        self.assertEqual(res, true_bounds)
+
+    def test_find_tokens_in_text_pos03(self):
+        s = 'Мама, мы все тяжело больны. Мама, я знаю, мы все сошли с ума.'
+        tokens = ['мы', 'все']
+        true_bounds = (6, 12)
+        res = find_tokens_in_text(s, tokens, 4)
+        self.assertIsInstance(res, tuple)
+        self.assertEqual(len(res), 2)
+        self.assertIsInstance(res[0], int)
+        self.assertIsInstance(res[1], int)
+        self.assertEqual(res, true_bounds)
+
+    def test_find_tokens_in_text_pos04(self):
+        s = 'Мама, мы все тяжело больны. Мама, я знаю, мы все сошли с ума.'
+        tokens = ['мы', 'все']
+        true_bounds = (42, 48)
+        res = find_tokens_in_text(s, tokens, 8)
+        self.assertIsInstance(res, tuple)
+        self.assertEqual(len(res), 2)
+        self.assertIsInstance(res[0], int)
+        self.assertIsInstance(res[1], int)
+        self.assertEqual(res, true_bounds)
+
+    def test_find_tokens_in_text_pos05(self):
+        s = 'Мама, мы все тяжело больны. Мама, я знаю, мы все сошли с ума.'
+        tokens = ['с']
+        true_bounds = (55, 56)
+        res = find_tokens_in_text(s, tokens)
+        self.assertIsInstance(res, tuple)
+        self.assertEqual(len(res), 2)
+        self.assertIsInstance(res[0], int)
+        self.assertIsInstance(res[1], int)
+        self.assertEqual(res, true_bounds)
+
+    def test_find_tokens_in_text_neg01(self):
+        s = 'Мама мыла раму.'
+        tokens = ['мама', 'мыла']
+        with self.assertRaises(RuntimeError):
+            _ = find_tokens_in_text(s, tokens, 3)
+
+    def test_find_tokens_in_text_neg02(self):
+        s = 'Мама мыла раму.'
+        tokens = ['мама', 'мы']
+        with self.assertRaises(RuntimeError):
+            _ = find_tokens_in_text(s, tokens)
+
+    def test_find_tokens_in_text_neg03(self):
+        s = 'Мама мыла раму.'
+        tokens = []
+        with self.assertRaises(RuntimeError):
+            _ = find_tokens_in_text(s, tokens)
+
+    def test_find_tokens_in_text_neg04(self):
+        s = ''
+        tokens = ['мама', 'мыла']
+        with self.assertRaises(RuntimeError):
+            _ = find_tokens_in_text(s, tokens)
+
+    def test_remove_oscillatory_hallucinations_pos01(self):
+        source_text = 'Сто процедуры безопасности'
+        true_text = 'Сто процедуры безопасности'
+        res = remove_oscillatory_hallucinations(source_text)
+        self.assertIsInstance(res, str)
+        self.assertEqual(res, true_text)
+
+    def test_remove_oscillatory_hallucinations_pos02(self):
+        source_text = ('Сто процедуры безопасности безопасности безопасности безопасности безопасности безопасности '
+                       'безопасности безопасности безопасности безопасности безопасности безопасности безопасности '
+                       'безопасности безопасности безопасности безопасности безопасности безопасности безопасности '
+                       'безопасности безопасности безопасности безопасности безопасности безопасности безопасности '
+                       'безопасности безопасности безопасности безопасности безопасности безопасности безопасности '
+                       'безопасности безопасности безопасности безопасности безопасности безопасности безопасности '
+                       'безопасности безопасности безопасности безопасности безопасности безопасности безопасности '
+                       'безопасности безопасности безопасности безопасности безопасности безопасности безопасности '
+                       'безопасности безопасности безопасности безопасности безопасности безопасности безопасности '
+                       'безопасности безопасности безопасности, безопасности.')
+        true_text = 'Сто процедуры безопасности.'
+        res = remove_oscillatory_hallucinations(source_text)
+        self.assertIsInstance(res, str)
+        self.assertEqual(res, true_text)
 
 
 if __name__ == '__main__':
