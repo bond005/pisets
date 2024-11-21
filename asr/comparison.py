4715fb181b88f6d023f0a8dbda7d0babd9056552
Oleg Sedukhin <sedol.mong@mail.ru>
2024-11-21 08:51:53 +0300

    docstring update

--- asr/comparison.py
+++ asr/comparison.py
@@ -78,7 +78,9 @@ def compare(text1: str, text2: str) -> list[CorrectionSuggestion]:
     - text2: another ASR prediction
     
     Returns a list of suggestions to replace, delete or insert something in the `text1`,
-    based on the difference between both texts.
+    based on the difference between both texts. So, this function is not symmetric,
+    since output suggestions contain positions in the `text1`. Punctuation is not compared,
+    so the punctiation from `text2` is knever used.
 
     Example:
     ```
